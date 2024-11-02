ba5faf9ee4f3b727cc38979df473d7aace9f3ad3
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2024-11-02 19:56:26 -0400

    remove uneccessary migration again

--- prisma/migrations/20241102182815_new_uniqueness_code_template/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title,userId]` on the table `CodeTemplate` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "CodeTemplate_title_userId_key" ON "CodeTemplate"("title", "userId");
