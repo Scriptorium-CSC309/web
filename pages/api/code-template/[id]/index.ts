178f793cbce5f14a5dea209d2d15e2893fa198ff
jerryy2021 <yanjerry1019@gmail.com>
2024-11-03 12:30:59 -0500

    fixed API request format for delete and update code tempate

--- pages/api/code-template/[id]/index.ts
+++ pages/api/code-template/[id]/index.ts
@@ -8,16 +8,12 @@ import getCodeTemplatesInteractor from "@/src/code-template/fetch-code-templates
 
 
 function handler(req: AuthenticatedRequest, res: NextApiResponse) {
-    if (req.method === "GET") {
-       return getCodeTemplatesInteractor(req, res);
-    } else if (req.method === "POST") {
-        return saveCodeTemplateInteractor(req, res);
-    } else if (req.method === "PATCH") {
+    if (req.method === "PUT") {
         return updateCodeTemplateInteractor(req, res);
     } else if (req.method === "DELETE") {
         return deleteCodeTemplateInteractor(req, res);
     } else {
-        res.setHeader("Allow", ["GET", "POST"]);
+        res.setHeader("Allow", ["PUT", "DELETE"]);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
