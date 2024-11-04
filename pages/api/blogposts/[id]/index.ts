9939f0e65de5a98c17b8014840f60605011d942b
jerryy2021 <yanjerry1019@gmail.com>
2024-11-03 22:43:44 -0500

    finished hide blogpost feature; added return statement for getBlogposts

--- pages/api/blogposts/[id]/index.ts
+++ pages/api/blogposts/[id]/index.ts
@@ -1,5 +1,6 @@
 import deleteBlogPostInteractor from "@/src/blogposts/[id]/delete-blogpost";
 import editBlogPostInteractor from "@/src/blogposts/[id]/edit-blogpost";
+import hideBlogPostInteractor from "@/src/blogposts/[id]/hide-blogpost";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { AuthenticatedRequest } from "@/src/auth/utils";
 
@@ -8,6 +9,8 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
         return deleteBlogPostInteractor(req, res);
     } else if (req.method === "PUT") {
         return editBlogPostInteractor(req as AuthenticatedRequest, res);
+    } else if (req.method == "PATCH") {
+        return hideBlogPostInteractor(req as AuthenticatedRequest, res);
     } else {
         res.setHeader("Allow", ["DELETE", "PUT"]);
         return res.status(405).end(`Method ${req.method} Not Allowed`);
