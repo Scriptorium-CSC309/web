310ea7c8a57876abd1e083c4bc967b6f31120640
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2024-11-03 23:15:05 -0500

    added optional auth

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse, NextApiHandler } from "next";
 import jwt from "jsonwebtoken";
-import { AuthenticatedHandler, AuthenticatedRequest } from "./utils";
+import {
+    AuthenticatedHandler,
+    AuthenticatedRequest,
+    OptionallyAuthenticatedHandler,
+    OptionallyAuthenticatedRequest,
+} from "./utils";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 /**
  * A Functional decorator to enable authorization and authentication.
  * @param handler The handler to add auth to.
- * @param options Options for the authorization and authentication.
+ * @param options - Optional settings for authentication and authorization.
+ *   - `admin` (default: `false`):
+ *          When set to `true`, the route will only allow access to authenticated users with admin privileges.
+ *   - `optional` (default: `true`):
+ *          When set to `true`, the route allows unauthenticated requests.
+ *          If authentication fails or is missing, `req.user` will be set to `null`.
  * @returns A modified handler with authorization and authentication implemented as specified by options.
  */
 export function withAuth<T>(
@@ -44,11 +54,7 @@ export function withAuth<T>(
                 isAdmin: decoded.isAdmin,
             };
 
-
-            await handler(
-                authReq,
-                res as NextApiResponse<T>
-            );
+            await handler(authReq, res as NextApiResponse<T>);
         } catch (error) {
             if (error instanceof jwt.TokenExpiredError) {
                 res.status(401).json({ error: "Access token expired" });
@@ -58,5 +64,46 @@ export function withAuth<T>(
             }
             return;
         }
+    };
+}
+
+/**
+ * A Functional decorator to optionally enable authorization and authentication.
+ * @param handler The handler to add optional auth to.
+ * @param options Options for the authorization and authentication.
+ * @returns A modified handler with optional authorization and authentication.
+ */
+export function withOptionalAuth<T>(
+    handler: OptionallyAuthenticatedHandler<T>
+): NextApiHandler<T | { error: string }> {
+    return async (
+        req: NextApiRequest,
+        res: NextApiResponse<T | { error: string }>
+    ): Promise<void> => {
+        let user = null;
+
+        const authHeader = req.headers.authorization;
+
+        if (authHeader && authHeader.startsWith("Bearer ")) {
+            const token = authHeader.substring(7); // Remove 'Bearer ' from 'Bearer token'
+
+            try {
+                const decoded = jwt.verify(token, JWT_SECRET) as any;
+
+                user = {
+                    userId: decoded.id,
+                    isAdmin: decoded.isAdmin,
+                };
+            } catch (error) {
+                console.log(error);
+                // Token is invalid or expired, user remains null
+            }
+        }
+
+        const authReq = req as OptionallyAuthenticatedRequest;
+        authReq.user = user;
+
+        // Call the handler
+        await handler(authReq, res as NextApiResponse<T>);
     };
 }
