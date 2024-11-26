9b4b19a778084788b9ffd520f58df3dd3898c541
jerryy2021 <yanjerry1019@gmail.com>
2024-11-26 12:29:26 -0500

    added minor modification: prevent non-admin user to see admin page before useEffect

--- frontend/utils/auth.tsx
+++ frontend/utils/auth.tsx
@@ -1,24 +1,28 @@
 import { useContext, useEffect } from "react";
 import { useRouter } from "next/router";
 import { StateContext as UserStateContext } from "@/frontend/contexts/UserContext";
+import { useState } from "react";
 
 /* Wrapper for components that need authenticated to be enfored */
 const withAuth = (WrappedComponent: React.ComponentType, admin?: boolean) => {
     const AuthWrapper = (props: any) => {
         const user = useContext(UserStateContext);
         const router = useRouter();
+        const [isLoading, setIsLoading] = useState(true);
 
         useEffect(() => {
             if (!user) {
                 router.push("/auth/login");
             } else if (admin && !user.isAdmin) {
                 router.push("/");
+            } else {
+                setIsLoading(false);
             }
         }, [user, router]);
 
         // Prevent rendering the wrapped component until the user is verified
-        if (!user) {
-            return null;
+        if (isLoading) {
+            return null; // Or render a loading spinner if desired
         }
 
         return <WrappedComponent {...props} />;
