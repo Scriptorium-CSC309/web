d08a51c58e71b50efffcef64ca967b2adc57b3e1
jerryy2021 <yanjerry1019@gmail.com>
2024-11-26 20:15:55 -0500

    finished manage-blogposts

--- constants.ts
+++ constants.ts
@@ -11,6 +11,7 @@ export const LANGUAGES = [
   "Go"
 ];
 export const NUM_AVATARS = 5;
+export const ITEMS_PER_PAGE = 10;
 export const VALID_PHONE_NUMBER = /^\+?[1-9]\d{1,14}$/; // E.164 phone format
 export const MAX_CHARS_TITLE_DESCRIPTION = 500;
 export const MAX_CHARS_CONTENT = 10000;
